20f6574ba79eabea720431d95398bc596301797c
tim sweeney <tsweeney@apple.com>
2015-04-13 19:55:50 -0700

    Initial Commit

---
+++ GS App 2 Business Card/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GS App 2 Business Card
+//
+//  Created by Tim Sweeney on 4/13/15.
+//  Copyright (c) 2015 Tim Sweeney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
